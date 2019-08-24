c0919ef778876540150cd68a416c839929835054
Harsha Manoj <danninamanoj123@gmail.com>
2019-08-24 12:42:45 +0530

    Add files via upload

---
+++ SocketUtils.py
@@ -0,0 +1,60 @@
+
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 24 19:13:16 2019
+
+@author: HARSHA
+"""
+
+
+
+# # SocketUtils
+
+# In[1]:
+
+import socket
+import pickle
+import select
+
+
+# In[2]:
+
+TCP_PORT = 5005
+BUFFER_SIZE = 1024
+
+def newServerConnection(ip_addr,port=TCP_PORT):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((ip_addr, port))
+    s.listen()
+    return s
+
+def recvObj(socket):
+    inputs, outputs, errs = select.select([socket],[],[socket],10)
+    if socket in inputs:
+        new_sock, addr = socket.accept()
+        all_data = b''
+        while True:
+            data =  new_sock.recv(BUFFER_SIZE)
+            if not data: break
+            all_data = all_data + data
+        return pickle.loads(all_data)
+    return None
+
+def sendObj(ip_addr, inObj, port=TCP_PORT):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((ip_addr, port))
+    data = pickle.dumps(inObj)
+    s.send(data)
+    s.close()
+    return False
+
+
+# In[3]:
+
+if __name__=="__main__":
+    server = newServerConnection('localhost')
+    O = recvObj(server)
+    print("Success! server established")
+    #print(O)
+    server.close()
+
